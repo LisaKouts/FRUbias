59f910d524aff8736ffe4bb272d9293428eb3e27
liza.koutsoviti@gmail.com <LisaKouts>
2023-11-23 17:47:32 +0100

    init added dot

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from FRSpy import FRSpy
-from h5file_create_array import Writearray
-from uncertainty import uncertainty
+from .FRSpy import FRSpy
+from .h5file_create_array import Writearray
+from .uncertainty import uncertainty
