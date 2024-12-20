30622b63fb5f34ea7d7b1186e42d8c6adf4ae1b1
LisaKouts <liza.koutsoviti@gmail.com>
2024-12-20 15:20:55 +0100

    added hvdm and radzikowska

--- FRSpy.py
+++ FRSpy.py
@@ -4,26 +4,42 @@ from tqdm import tqdm
 
 class FRSpy:
 
-    def __init__(self, target, membership):
+    def __init__(self, membership, implication = 'Luka', conjunction = 'Luka', frs_method = 'Radzikowska'):
+        '''
+        Compute the membership values to the fuzzy-rough regions
+
+        Attributes
+        ----------
+        implication: string
+            options are 'Luka', 'Godel', 'Fodor', 'Goguen' (see publication for details)
+        conjunction: string
+            options are 'Standard', 'Algebraic', 'Luka', 'Drastic' (see publication for details)
+        '''
+        self.target = len(membership.columns)
         self.membership = membership.to_numpy().astype('int32')
-        self.target = target.astype('int32')
+        self.im = implication
+        self.con = conjunction
+        self.frs_method = frs_method
 
     def regions(self, h5file, key, hide_progress):
 
-        POS = np.zeros((len(np.unique(self.target)), len(self.target)))
-        NEG = np.zeros((len(np.unique(self.target)), len(self.target)))
-        BND = np.zeros((len(np.unique(self.target)), len(self.target)))
+        POS = np.zeros((self.target, len(self.membership)))
+        NEG = np.zeros((self.target, len(self.membership)))
+        BND = np.zeros((self.target, len(self.membership)))
         
         with hp.File(h5file, "r") as f:
-            for instance in tqdm(f[key].keys(), desc=key+' computing Membership Values', disable=hide_progress): # iterating through rows
+            for instance in tqdm(f[key].keys(), desc=key+' computing Membership Values', disable=hide_progress, miniters=1000): # iterating through rows
                 i = int(instance[3:])
                 distance = f[key][instance][:]
-                for k in np.unique(self.target):
-                    POS[k][i], NEG[k][i], BND[k][i] = self.process_object(i, k, distance)
+                for k in range(self.target):
+                    if self.frs_method == 'Inuiguchi':
+                        POS[k][i], NEG[k][i], BND[k][i] = self.process_object_inuiguchi(i, k, distance)
+                    if self.frs_method == 'Radzikowska':
+                        POS[k][i], NEG[k][i], BND[k][i] = self.process_object_radzikowska(k, distance)
 
         return [POS, NEG, BND]
 
-    def process_object(self, i, k, distance):
+    def process_object_inuiguchi(self, i, k, distance):
 
         # lower approximation
         fuzzy_relation_i_j = distance * self.membership[i,k]
@@ -38,12 +54,47 @@ class FRSpy:
         sup = max(supremum, self.membership[i,k])
 
         return inf, 1-sup, sup-inf
+    
+    def process_object_radzikowska(self, k, distance):
+        
+        # lower approximation
+        fuzzy_implication = self.implicator(distance, self.membership[:,k])
+        inf = min(1, fuzzy_implication)
+        
+        # upper approximation
+        fuzzy_conjunction = self.conjunction(distance, self.membership[:,k])
+        sup = max(0, fuzzy_conjunction)
+
+        return inf, 1-sup, sup-inf
 
     def implicator(self, a, b):
-        return min(np.min(1 - a + b), 1)
+        if self.im == 'Luka':
+            return min(np.min(1 - a + b), 1)
+        
+        if self.im == 'Fodor':
+            return min(np.where(a <= b, 1, np.maximum(1-a,b)))
+
+        if self.im == 'Godel':
+            return min(np.where(a <= b, 1, b))
+        
+        if self.im == 'Goguen':
+            from numpy import inf
+            goguen = np.where(a <= b, 1, b/a)
+            goguen[goguen == inf] = 0
+            return min(goguen)
 
     def conjunction(self, a, b):
-        return max(np.max(a + b - 1), 0)
+        if self.con == 'Luka':
+            return max(np.max(a + b - 1), 0)
+        
+        if self.con == 'Standard':
+            return max(np.minimum(a,b))
+        
+        if self.con == 'Drastic':
+            return max(np.maximum(np.where(b==1, a, 0),np.where(a==1, b, 0)))
+        
+        if self.con == 'Algebraic':
+            return max(a*b)
 
 import sys
 if __name__=="__main__":
--- fuzzy_rough_regions.py
+++ fuzzy_rough_regions.py
@@ -5,86 +5,149 @@ from .FRSpy import FRSpy
 from .h5file_create_array import Writearray
 import h5py
 import pickle
-import os 
+import os
 
-def compute_membership_values(df, case_name, path, columns, complete = False, hide_progress = False):
-  '''
-  A function that computes the membership values to fuzzy-rough regions.
+class FRU:
 
-  Attributes
-  ------------
-  df : pandas DataFrame
-    Contains all variables in the dataset plus the decision variable which must be located in the LAST column
-  
-  case_name : str
-    name of the h5 file
+  def __init__(self, df, path):
+    '''
+    Attributes
+    ------------
+    df : pandas DataFrame
+      Contains all variables in the dataset plus the decision variable
+ 
+    path : str
+      path to file where the membership values to fuzzy-rough regions should be saved in pickle form. Pickle format is used for storage efficiency in case of large datasets
+      can also be empty and the file will be saved in the current directory
+    '''
+
+
+    self.df = df
+    self.path = path
+
+  def membership_values(self, 
+                        columns, 
+                        target,
+                        h5_name, 
+                        similarity = 'HMOM', 
+                        alpha = 0.5, 
+                        implication = 'Luka',
+                        conjunction = 'Luka',
+                        frs_method = 'Inuiguchi',
+                        complete = False, 
+                        hide_progress = False):
+    '''
+    A function that computes the membership values to fuzzy-rough regions for larger datasets. It computes a similarity matrix and stores it in a h5 file. 
+    It also computes the fuzzy-rough regions and saves them in a pickle file.
+
+    Attributes
+    ------------
+    columns : list of strings
+      a list of the column names to be suppressed or removed from the dataset. after removal, the fuzzy-rough
+      set regions will be computed using the rest of the data
     
-  path : str
-    path to file
-  
-  columns: list of strings
-    a list of the column names to be suppressed or removed from the dataset. after removal, the fuzzy-rough
-    set regions will be computed using the rest of the data
-  
-  complete : False
-    if True, the membership values to the fuzzy-rought regions are create using the complete set of data. 
-    if this object already exists, you can set it to False to save time if dealing with large dataset
-  '''
+    target : string
+      name of target variable, outcome, label
+      
+    h5_name : str
+      name of the h5 file where the similarity values are saved
+    
+    similarity: string
+      Heterogeneus distance functions able to process both numeric and nominal features
+      Cane be either 'HMOM', 'HEOM' or 'HVDM'; see journal paper Fuzzy-Rough Cognitive Networks (Nápoles et al., 2018) for the equations
 
-  # rename the column names cause they contain illegal characters and h5 cannot index columns properly
-  df.columns = list(map(lambda x: re.sub('=<', 'less',x), list(df.columns)))
-  df.columns = list(map(lambda x: re.sub('=>', 'more',x), list(df.columns)))
-  df.columns = list(map(lambda x: re.sub('/|\(|\)|>|<|=| ', '',x), list(df.columns)))
+    alpha: float
+      parameter to separate the fuzzy-rough regions
 
-  # fr values to fuzzy rough regions
+    implication: string
+      fuzzy implication, options are 'Luka', 'Godel', 'Fodor', 'Goguen' (see publication for details)
 
-  target = df.iloc[:,-1]
-  membership = pd.get_dummies(target)
-  file_name = os.path.join(path, "matrix_"+case_name+".h5")
+    conjunction: string
+      fuzzy conjunction, options are 'Standard', 'Algebraic', 'Luka', 'Drastic' (see publication for details)
 
-  if not complete:
-    h5file = open_file(file_name, mode="w", title=case_name) # create h5 file to store distance matrix
+    frs_method: string
+      definition of fuzzy-rough sets, options are either Inuiguchi or Radzikowska
 
-    group = h5file.create_group("/", 'full', 'Distances after removing full') # full
-    Writearray(df.iloc[:,:-1], 0.5, 'full').sim_array(h5file = h5file, group = group, hide_progress = hide_progress)
+    complete: boolean
+      if True, the membership values to the fuzzy-rought regions are created using the complete set of data. 
+      if this object already exists, you can set it to False to save time if dealing with large dataset
+    
+    '''
 
-    h5file.close()
+    # rename the column names cause they contain illegal characters and h5 cannot index columns properly
+    self.df.columns = list(map(lambda x: re.sub('=<', 'less',x), list(self.df.columns)))
+    self.df.columns = list(map(lambda x: re.sub('=>', 'more',x), list(self.df.columns)))
+    self.df.columns = list(map(lambda x: re.sub('/|\(|\)|>|<|=| ', '',x), list(self.df.columns)))
 
-    h5file = open_file(file_name, mode="r")
-    frregions = FRSpy(target, membership).regions(file_name,'full', hide_progress = hide_progress)
+    # fr values to fuzzy rough regions
+    membership = pd.get_dummies(self.df[target])
+    file_name = os.path.join(self.path, "matrix_"+h5_name+".h5")
 
-    file_name_pickle = os.path.join(path, 'full_mem.pickle')
-    with open(file_name_pickle, 'wb') as handle:
-      pickle.dump(frregions, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    h5file.close()
+    if not complete:
+      h5file = open_file(file_name, mode="w", title=h5_name) # create h5 file to store distance matrix
 
-    h5file = h5py.File(file_name, mode="a")
-    del h5file['full']
-    h5file.close()
+      group = h5file.create_group("/", 'full', 'Distances after removing full') # full
+      Writearray(self.df[columns], alpha, 'full', method=similarity, target=self.df[target]).sim_array(h5file = h5file, group = group, hide_progress = hide_progress)
 
-  for s_attr in columns:
-    # if hide_progress:
-    #   print(s_attr)
+      h5file.close()
 
-    h5file = open_file(file_name, mode="a")
-    dataset = df.iloc[:,:-1].drop(s_attr, axis=1) # remove protected
-    group = h5file.create_group("/", s_attr, 'Distances after removing '+s_attr)
-    Writearray(dataset, 0.5, s_attr).sim_array(h5file = h5file, group = group, hide_progress = hide_progress)
+      h5file = open_file(file_name, mode="r")
+      frregions = FRSpy(membership, implication = implication, conjunction = conjunction, frs_method = frs_method).regions(file_name,'full', hide_progress = hide_progress)
 
-    h5file.close()
-    h5file = open_file(file_name, mode="r")
+      file_name_pickle = os.path.join(self.path, 'full_mem.pickle')
+      with open(file_name_pickle, 'wb') as handle:
+        pickle.dump(frregions, handle, protocol=pickle.HIGHEST_PROTOCOL)
+      h5file.close()
 
-    frregions = FRSpy(target, membership).regions(file_name,s_attr, hide_progress = hide_progress)
-    file_name_pickle = os.path.join(path, s_attr+'_mem.pickle')
-    with open(file_name_pickle, 'wb') as handle:
-      pickle.dump(frregions, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    h5file.close()
+      h5file = h5py.File(file_name, mode="a")
+      del h5file['full']
+      h5file.close()
 
-    h5file = h5py.File(file_name, mode="a")
-    del h5file[s_attr]
-    h5file.close()
+    for s_attr in columns:
+      # if hide_progress:
+      #   print(s_attr)
+
+      h5file = open_file(file_name, mode="a")
+      dataset = self.df[columns].drop(s_attr, axis=1) # remove protected
+      group = h5file.create_group("/", s_attr, 'Distances after removing '+s_attr)
+      Writearray(dataset, alpha, s_attr, method=similarity, target=self.df[target]).sim_array(h5file = h5file, group = group, hide_progress = hide_progress)
+
+      h5file.close()
+      h5file = open_file(file_name, mode="r")
+
+      frregions = FRSpy(membership, implication = implication, conjunction = conjunction, frs_method = frs_method).regions(file_name,s_attr, hide_progress = hide_progress)
+      file_name_pickle = os.path.join(self.path, s_attr+'_mem.pickle')
+      with open(file_name_pickle, 'wb') as handle:
+        pickle.dump(frregions, handle, protocol=pickle.HIGHEST_PROTOCOL)
+      h5file.close()
+
+      h5file = h5py.File(file_name, mode="a")
+      del h5file[s_attr]
+      h5file.close()
+
+  def load_membership_values(self):
+    '''
+    Loads the membership values to fuzzy-rough regions for larger datasets from the pickle files. It should be called after function membership_values()
+
+    Returns
+    ------------
+    Dictionary object: each entry in the dictionary contains the membership values to the fuzzy rough regions of each pickle file in the folder
+    The values are stored in numpy arrays for each decision class 
+    '''
+    mem_dic = {}
+    for root, _, files in os.walk(self.path):
+        for name in files:
+            if 'pickle' in name:
+                att_name = re.findall(r"(.*?)_mem.pickle", name)[0]
+                file_path_name = os.path.join(root, name) 
+                with open(file_path_name, 'rb') as handle: 
+                    mem_dic_att = pickle.load(handle)
+                    mem_dic[att_name] = mem_dic_att
+
+    return mem_dic
+
 
 import sys
 if __name__=="__main__":
-  args = compute_membership_values(sys.argv)
+  args = FRU(sys.argv).membership_values(sys.argv)
   print("In mymodule:",args)
